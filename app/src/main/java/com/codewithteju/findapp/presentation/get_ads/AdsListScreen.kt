bfb074d20f6f555a6b1eb5f2325191f0b6ef706c
Tejaswee Shah <tejaswee.shah@gmail.com>
2023-06-20 15:28:59 -0400

    Compose - AdsListScreen

---
+++ app/src/main/java/com/codewithteju/findapp/presentation/get_ads/AdsListScreen.kt
@@ -0,0 +1,68 @@
+package com.codewithteju.findapp.presentation.get_ads
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.codewithteju.findapp.presentation.get_ads.components.AdsListItem
+
+
+@Composable
+fun AdsListScreen(
+    modifier: Modifier,
+    adsListViewModel: AdsListViewModel = hiltViewModel()
+) {
+    val adsListState = adsListViewModel.adsListState.value
+    //val adsListState by remember{ adsListViewModel.adsListState}
+
+    Box(
+        modifier = modifier.fillMaxSize()
+    ) {
+
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(16.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            items(adsListState.ads) { ad ->
+                AdsListItem(
+                    advertisement = ad,
+                    /*onItemClick = {
+                        Log.i("TAG", "Navigate to Ad Details Screen")
+                    }*/
+                )
+            }
+        }
+
+        if (adsListState.error.isNotBlank()) {
+            Text(
+                text = adsListState.error,
+                color = MaterialTheme.colorScheme.error,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp)
+                    .align(Alignment.Center)
+            )
+        }
+        if (adsListState.isLoading) {
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+        }
+    }
+}
