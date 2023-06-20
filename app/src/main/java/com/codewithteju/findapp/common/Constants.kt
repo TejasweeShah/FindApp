7a468d2dba5ecadcac8f8bffcd1719f269bef58f
Tejaswee Shah <tejaswee.shah@gmail.com>
2023-06-20 15:11:49 -0400

    Constants

---
+++ app/src/main/java/com/codewithteju/findapp/common/Constants.kt
@@ -0,0 +1,8 @@
+package com.codewithteju.findapp.common
+
+object Constants {
+
+    const val API_URL = "https://gist.githubusercontent.com/baldermork/6a1bcc8f429dcdb8f9196e917e5138bd/raw/"
+    const val IMAGE_BASE_URL = "https://images.finncdn.no/dynamic/480x360c/"
+
+}
