7f22d5ee980ca834534183f881b87444b02ff834
Tejaswee Shah <tejaswee.shah@gmail.com>
2023-06-20 15:16:22 -0400

    Advertisement Model Data Class

---
+++ app/src/main/java/com/codewithteju/findapp/domain/model/Advertisement.kt
@@ -0,0 +1,9 @@
+package com.codewithteju.findapp.domain.model
+
+data class Advertisement(
+    val id: String,
+    val title: String,
+    val location: String,
+    val price: Int,
+    val imageUrl: String
+)
